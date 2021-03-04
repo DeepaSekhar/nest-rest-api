e959b9a8bb3be9108f9d792597052b74ad058eb4
deepaakhil84 <deepaakhiltvm@gmail.com>
2021-03-04 23:20:56 +0000

    creating a module for the item controller

---
+++ src/items/items.module.ts
@@ -0,0 +1,12 @@
+
+import { ItemsController } from './items.controller';
+import { ItemsService } from './items.service';
+import { Module } from '@nestjs/common';
+
+
+@Module({
+    imports: [],
+    controllers: [ItemsController],
+    providers: [ItemsService],
+})
+export class ItemModule { }
